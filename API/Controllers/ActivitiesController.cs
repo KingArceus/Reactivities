21af3abc9bdf7b9d0a82e92acb208895e3315021
KingArceus <khangtpc@gmail.com>
2024-12-12 19:39:34 +0700

    Add Seed and Base Controller

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,30 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataDbContext _context;
+        public ActivitiesController(DataDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities() 
+        {
+            var activities = await _context.Activities.ToListAsync();
+            return Ok(activities);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            var activity = await _context.Activities.FindAsync(id);
+            return Ok(activity);
+        }
+    }
+}
