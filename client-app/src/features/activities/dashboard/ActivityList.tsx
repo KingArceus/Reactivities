5e772d0590b63f79f6dc76b2d70fa688712ce115
KingArceus <khangtpc@gmail.com>
2025-01-02 22:43:08 +0700

    Modified Group Item

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,21 +1,28 @@
-import { Item, Segment } from "semantic-ui-react";
+import { Header } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
 import ActivityListItem from "./ActivityListItem";
+import { Fragment } from "react/jsx-runtime";
 
 function ActivityList() {
-    const {activityStore} = useStore();
+    const { activityStore } = useStore();
 
-    const {activitiesByDate} = activityStore;
+    const { groupedActivities } = activityStore;
 
     return (
-        <Segment>
-            <Item.Group divided>
-                {activitiesByDate.map(activity => (
-                    <ActivityListItem activity={activity} key={activity.id} />
-                ))}
-            </Item.Group>
-        </Segment>
+        <>
+            {groupedActivities.map(([group, activities]) => (
+                <Fragment key={group}>
+                    <Header sub color='teal'>
+                        {group}
+                    </Header>
+                    {activities.map(activity => (
+                        <ActivityListItem activity={activity} key={activity.id} />
+                    ))}
+                </Fragment>
+            ))}
+        </>
+
     )
 }
 
